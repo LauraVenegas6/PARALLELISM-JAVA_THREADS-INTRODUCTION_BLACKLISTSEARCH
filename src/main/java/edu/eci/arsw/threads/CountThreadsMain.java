e3dbb32c91fa72f8edd1f467a5399f80e4a06fb4
unknown <sergio.idarraga-t@labinfo.is.escuelaing.edu.co>
2026-01-23 09:47:24 -0500

    feat: Implementación de os hilos para cada intervalo del conjunto de numeros, además se verificó la diferencia entre start() y run()

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -13,8 +13,44 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
+        // Parte Prueba
         CountThread countThread1 = new CountThread(5, 10);
         countThread1.start();
+
+        //Parte 2 Nums [0, 99]
+        System.out.println("[0, 99] -----------------");
+        CountThread countThread2 = new CountThread(0, 99);
+        countThread2.run();
+
+        //Parte 3 Nums [99, 199]
+        System.out.println("[99, 199] -----------------");
+        CountThread countThread3 = new CountThread(99, 199);
+        countThread3.run();
+
+        //Parte 4 Nums [200, 299]
+        System.out.println("[200, 299] -----------------");
+        CountThread countThread4 = new CountThread(200, 299);
+        countThread4.run();
+
+
+        // // Parte Prueba
+        // CountThread countThread1 = new CountThread(5, 10);
+        // countThread1.start();
+
+        // //Parte 2 Nums [0, 99]
+        // System.out.println("[0, 99] -----------------");
+        // CountThread countThread2 = new CountThread(0, 99);
+        // countThread2.start();
+
+        // //Parte 3 Nums [99, 199]
+        // System.out.println("[99, 199] -----------------");
+        // CountThread countThread3 = new CountThread(99, 199);
+        // countThread3.start();
+
+        // //Parte 4 Nums [200, 299]
+        // System.out.println("[200, 299] -----------------");
+        // CountThread countThread4 = new CountThread(200, 299);
+        // countThread4.start();
     }
     
 }
